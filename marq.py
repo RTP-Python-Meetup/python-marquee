b212eeefe0763e151bdf9370344e72a16a74c13c
Malcolm VanOrder <mvanorder1390@gmail.com>
2018-11-03 03:21:25 +0000

    Load marquee from file
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- marq.py
+++ marq.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+
 import os
+import sys
 from time import sleep
 
 marq_text = """██████╗ ████████╗██████╗     ██████╗ ██╗   ██╗████████╗██╗  ██╗ ██████╗ ███╗   ██╗    ███╗   ███╗███████╗███████╗████████╗██╗   ██╗██████╗
@@ -12,14 +15,15 @@ class Marquee(object):
 
     def __init__(self, text):
         self.lines = tuple(
-            line.strip() for line in text.split('\n'))
+            line for line in text.split('\n'))
         self.width = max(tuple(len(line) for line in self.lines))
         term_size = os.popen('stty size', 'r').read().split()
         self.term_size = (int(term_size[0]), int(term_size[1]))
 
     def run(self):
         offset = 0
-        print()
+        print('\n' * (self.term_size[0] - 1) +
+                '\033[{}A'.format((self.term_size[0] + len(self.lines)) // 2))
         while True:
             for line in self.lines:
                 if offset < 0:
@@ -33,7 +37,17 @@ class Marquee(object):
                 offset = 0 - self.term_size[1]
             print("\033[1A\033[K" * (len(self.lines) + 1))
         
+def main():
+    if len(sys.argv) > 1:
+        marq_filename = sys.argv[1]
+        with open(marq_filename, 'r') as marq_file:
+            marq_text = marq_file.read()
+    marq = Marquee(marq_text)
+    marq.run()
 
-marq = Marquee(marq_text)
-
-marq.run()
+if __name__ == '__main__':
+    try:
+        exit(main())
+    except KeyboardInterrupt:
+        print()
+        exit()
